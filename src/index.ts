d41f6e66ad7034528d534ec9d996dfeac5c0a99d
Avior Kahalani <aviorkahalani@gmail.com>
2024-05-22 16:59:35 +0300

    feat: refactor for production

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import catRoutes from './api/cat.routes'
 
-const PORT = 3030
+const PORT = process.env.PORT || 3030
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'))
 
 // ROUTES
 app.use('/api/cat', catRoutes)
